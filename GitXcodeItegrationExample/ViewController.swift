9e947dfc311b57ae4858dbe8cc6f7b0ffd511721
Warren Sistrom <auswaz@gmail.com>
2020-01-04 19:53:59 +1100

    Initial Commit

---
+++ GitXcodeItegrationExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitXcodeItegrationExample
+//
+//  Created by warren on 4/1/20.
+//  Copyright © 2020 Warren Sistrom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
