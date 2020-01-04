927afc2ae8f429bfa0846b1895c0bf52d060923d
Warren Sistrom <auswaz@gmail.com>
2020-01-04 20:33:41 +1100

    Log message on app launch Add button to UI that logs to console when tapped

--- GitXcodeItegrationExample/ViewController.swift
+++ GitXcodeItegrationExample/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
-
-
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+      print("This is a Git tutorial")
+    }
 }
 
